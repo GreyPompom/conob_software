c6bc888730c6ca8c5a4ab329bbde80c06f451312
Emely <emelysantina00@gmail.com>
2024-07-23 21:57:22 -0300

    feat: view politica

--- src/App.js
+++ src/App.js
@@ -3,19 +3,20 @@ import './App.css';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './components/Home/Home';
+import Politica from './components/Politica/Politica';
+import NavBar from './components/NavBar';
 function App() {
   return (
     <div className='App'>
-    <React.StrictMode>
-    <Router>
+     <React.StrictMode>
       <Routes>
          <Route path="/" element={<Home />} />
        {/* <Route path="/sistemas" element={<Sistemas />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/politica" element={<Politica />} /> */}
+        <Route path="/about" element={<About />} />*/}
+        <Route path="/politica" element={<Politica />} /> 
         {/* Adicione outras rotas aqui se necessário */}
       </Routes>
-    </Router>
+   
     </React.StrictMode>
     </div>
   );
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { NavLink, useLocation } from 'react-router-dom';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import logo from '../assets/logo/logo_conob_zoom.png';
 import './NavBar.css';
 
 function NavBar() {
+  const location = useLocation();
   return (
     <>
-      <nav className="navbar navbar-expand-lg bg-body-tertiary backdrop-blur-sm bg-white fixed-top">
+      <nav className="navbar navbar-expand-lg backdrop-blur-sm bg-white fixed-top  shadow-sm">
         <div className="container-fluid">
           <a className="navbar-brand" href="#">
             <img src={logo} alt="Logo" className="logo-navbar" />
@@ -17,21 +19,52 @@ function NavBar() {
           </button>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
-              <li className="nav-item">
-                <a className="nav-link custom-active" aria-current="page" href="#">Home</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link custom-active" href="#">Pagina</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link custom-active" href="#">Sistemas</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link custom-active" href="#">Sobre</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link custom-active" href="#">Politica</a>
-              </li>
+            <li className="nav-item">
+              <NavLink
+                exact
+                to="/"
+                className={`nav-link custom-active ${location.pathname === '/' ? 'active' : ''}`}
+                aria-current={location.pathname === '/' ? 'page' : undefined}
+              >
+                Home
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink
+                to="/pagina"
+                className={`nav-link custom-active ${location.pathname === '/pagina' ? 'active' : ''}`}
+                aria-current={location.pathname === '/pagina' ? 'page' : undefined}
+              >
+                Pagina
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink
+                to="/sistemas"
+                className={`nav-link custom-active ${location.pathname === '/sistemas' ? 'active' : ''}`}
+                aria-current={location.pathname === '/sistemas' ? 'page' : undefined}
+              >
+                Sistemas
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink
+                to="/sobre"
+                className={`nav-link custom-active ${location.pathname === '/sobre' ? 'active' : ''}`}
+                aria-current={location.pathname === '/sobre' ? 'page' : undefined}
+              >
+                Sobre
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink
+                to="/politica"
+                className={`nav-link custom-active ${location.pathname === '/politica' ? 'active' : ''}`}
+                aria-current={location.pathname === '/politica' ? 'page' : undefined}
+              >
+                Politica
+              </NavLink>
+            </li>
             </ul>
             <a className="btn btn-primary rounded-pill ms-2" href="#" role="button">
               Entre em contato <i className="bi bi-arrow-right-short"></i>
