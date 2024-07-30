242335aebf9c25bb64ddc746002a8b758998eb0f
Emely <emelysantina00@gmail.com>
2024-07-29 21:06:06 -0300

    feat:page 404

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './components/Home/Home';
 import Politica from './components/Politica/Politica';
+import Page404 from './components/404/404';
 import NavBar from './components/NavBar';
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
         <Route path="/about" element={<About />} />*/}
         <Route path="/politica" element={<Politica />} /> 
         {/* Adicione outras rotas aqui se necessário */}
+        <Route path="*" element={<Page404 />} />
       </Routes>
    
     </React.StrictMode>
